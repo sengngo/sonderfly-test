c8b7d5dc1d086d5e67b477fe94801966e7455f83
Seng Ngo <sengngo@gmail.com>
2015-12-04 02:54:29 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sonderfly::Application.config.secret_key_base = '884ba6e06dc702957fde576d4139b2f05fe427911dcda502e1027b76727ee25c4a476256929756043059e641d0ab40cf28c7fd6b073a8cb9b9a616ca10c289f7'
