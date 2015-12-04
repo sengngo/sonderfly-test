28a07f714932097d01d061062c34de66bb59e5e7
Seng Ngo <sengngo@gmail.com>
2015-12-04 03:00:52 +0000

    added post controller and model with routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+	def index
+	end
+
+	def show
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def edit
+	end
+
+	def destroy
+	end
+
+	private
+
+	def post_params
+		
+	end
+
+end
